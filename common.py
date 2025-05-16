430c5b4b783967782740920b482607c411bf8006
Dmitrii Efimenko <dmitriiaefimenko@gmail.com>
2025-05-16 08:41:24 +0100

    Small tweaks to common tools and demo

--- common.py
+++ common.py
@@ -5,7 +5,6 @@ from tkinter import Tk, Canvas, Label, StringVar, mainloop
 WINDOW_SIZE = (800, 960)
 CANVAS_SIZE = (800, 800)
 TEXT_SIZE = (800, 160)
-FPS = (24, 1/24)
 
 class Object:
     def __init__(self, title, center_x, center_y, radius, color, speed):
@@ -38,18 +37,18 @@ def _redraw_label(label, string_var, data):
     string_var.set(data)
     label.update()
 
-def _fps_pause(start_time):
+def _fps_pause(frame_time, start_time):
     diff = time.time() - start_time
-    sleep(0 if diff >= FPS[1] else FPS[1] - diff)
+    sleep(0 if diff >= frame_time else frame_time - diff)
 
 def main_loop(title, calculate_data_func):
     canvas, label, string_var = _init_window(title)
     while True:
         start_time = time.time()
-        data, description, done = calculate_data_func()
+        data, description, frame_time, done = calculate_data_func()
         _redraw_canvas(canvas, data)
         _redraw_label(label, string_var, description)
-        _fps_pause(start_time)
+        _fps_pause(frame_time, start_time)
         if done:
             break
     mainloop()
--- demo.py
+++ demo.py
@@ -1,21 +1,27 @@
-from common import main_loop, Object, CANVAS_SIZE, FPS
+from common import main_loop, Object, CANVAS_SIZE
 
+FPS = (24, 1/24)
+SCALE = 5
 G = 9.8
 
-objects = [Object("Object", CANVAS_SIZE[0] // 2, 15, 15, "blue", 0)]
+objects = [
+    Object("Object BLUE", CANVAS_SIZE[0] // 2, 15, 15, "blue", 0),
+    Object("Object RED", CANVAS_SIZE[0] // 4, CANVAS_SIZE[1] - 15, 15, "red", -8)
+]
 
 def calculate_data_func():
     global objects
-    done = False
+    description = ""
+    done = True
     for obj in objects:
         obj.speed += G / FPS[0]
-        if obj.center_y + obj.speed > CANVAS_SIZE[1] - obj.radius:
+        if obj.center_y + SCALE * obj.speed > CANVAS_SIZE[1] - obj.radius:
             obj.center_y = CANVAS_SIZE[1] - obj.radius
             obj.speed = 0
-            done = True
         else:
-            obj.center_y = obj.center_y + obj.speed
-    description = f"x:{objects[0].center_x:.2f}, y:{CANVAS_SIZE[1] - objects[0].center_y:.2f}, speed:{objects[0].speed:.2f}"
-    return objects, description, done
+            obj.center_y = obj.center_y + SCALE * obj.speed
+            done = False
+        description += f"{obj.title}\tx: {obj.center_x:.2f}, y: {CANVAS_SIZE[1] - obj.center_y:.2f}, speed: {obj.speed:.2f}\n"
+    return objects, description, FPS[1], done
 
 main_loop("Demo", calculate_data_func)
